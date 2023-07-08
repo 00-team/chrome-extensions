ccd9869f0b29a80d2b549cf8f1db91d957a9a94d
007 <dr007cc@gmail.com>
2023-07-08 16:04:37 +0330

    fix: update load from file

--- mason/src/components/Load.tsx
+++ mason/src/components/Load.tsx
@@ -1,7 +1,27 @@
 import React, { FC } from 'react'
 
+import { useSetAtom } from 'jotai'
+import { TargetAtom } from 'state'
+
 const Load: FC = () => {
-    return <button>Load Plan</button>
+    const setTargets = useSetAtom(TargetAtom)
+
+    const load = () => {
+        let file = document.createElement('input')
+        file.type = 'file'
+        file.accept = 'application/json'
+
+        file.onchange = async () => {
+            if (!file.files || !file.files.length) return
+            const data = JSON.parse(await file.files[0]!.text())
+            if (!Array.isArray(data)) return alert('invalid plan.json')
+            setTargets(data)
+        }
+
+        file.click()
+    }
+
+    return <button onClick={load}>Load Plan</button>
 }
 
 export { Load }
