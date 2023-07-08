56ed17a4fea666abf513dbfb11eafb8d53a89e7f
007 <dr007cc@gmail.com>
2023-07-08 16:11:57 +0330

    fix: update save component

--- mason/src/components/Save.tsx
+++ mason/src/components/Save.tsx
@@ -1,7 +1,26 @@
 import React, { FC } from 'react'
 
+import { useAtomValue } from 'jotai'
+import { TargetAtom } from 'state'
+
 const Save: FC = () => {
-    return <button>Save Plan</button>
+    const targets = useAtomValue(TargetAtom)
+
+    const save = () => {
+        let url = URL.createObjectURL(
+            new Blob([JSON.stringify(targets, null, 4) + '\n'], {
+                type: 'application/json',
+            })
+        )
+
+        chrome.downloads.download({
+            url: url,
+            saveAs: true,
+            filename: 'plan.json',
+        })
+    }
+
+    return <button onClick={save}>Save Plan</button>
 }
 
 export { Save }
